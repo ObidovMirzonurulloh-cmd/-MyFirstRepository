ccf58e603a8935603b1b66be93b2cbd361413b18
Name <mirzonurullo@example.ru>
2026-03-22 09:43:34 +0500

    program-14.3.3

---
+++ ItogProgram.14.3.3/ConsoleApp10/Model/Contacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10.Model
+{
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) 
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ ItogProgram.14.3.3/ConsoleApp10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp10.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sorts = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName);
+
+            foreach (Contact contact in sorts) 
+            {
+                Console.WriteLine($"{contact.Name} {contact.LastName} {contact.PhoneNumber} {contact.Email}");
+            }
+        }
+    }
+    
+}
