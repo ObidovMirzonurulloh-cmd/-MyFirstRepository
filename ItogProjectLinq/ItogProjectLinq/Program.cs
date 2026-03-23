1b3fd791397d516586e8afcc5e394c49c0f02b80
Name <mirzonurullo@example.ru>
2026-03-23 16:19:40 +0500

    ok linq

---
+++ ItogProjectLinq/ItogProjectLinq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Масив классов
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        //абьеденяем все классы
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes
+                .SelectMany(c => c.Students)
+                .ToArray();
+        }
+
+        //класс со списком для хранение студентов
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
