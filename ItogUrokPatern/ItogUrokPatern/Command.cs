910879bc4033c23abc8dac1d93cdab1d75a212cc
Name <mirzonurullo@example.ru>
2026-04-07 20:52:01 +0500

    ok patern 2

---
+++ ItogUrokPatern/ItogUrokPatern/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+
+namespace ItogUrokPatern
+{
+    // Абстрактный класс для команд, использующий YoutubeClient
+    abstract class Command
+    {
+        protected YoutubeClient _client;
+
+        public Command(YoutubeClient client)
+        {
+            _client = client;
+        }
+
+        // Абстрактный метод для выполнения команды, который должен быть реализован в наследниках
+        public abstract Task Execute();
+    }
+}
---
+++ ItogUrokPatern/ItogUrokPatern/GetCommandDownload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+
+namespace ItogUrokPatern
+{
+    //Класс для команды скачивания видео с YouTube
+    class GetCommandDownload : Command
+   {
+        private string _url;
+
+        // Конструктор, принимающий клиента и URL видео
+        public GetCommandDownload(YoutubeClient client, string url) : base(client)
+        {
+            _url = url;
+        }
+
+        // Метод для выполнения команды скачивания видео
+        public override async Task Execute()
+        {
+            await _client.Videos.DownloadAsync(_url, "video.mp4", 
+                builder => builder.SetPreset(ConversionPreset.UltraFast)
+                .SetFFmpegPath("ffmpeg.exe")
+                );
+            Console.WriteLine("Видео скачано");
+            
+        }
+    }
+}
